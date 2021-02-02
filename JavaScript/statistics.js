f2d0377ed8c72c2b3054675ef3878591eb134226
mazzlabs <ma15fo43@gmail.com>
2021-02-02 18:21:42 +0100

    🎨 remade the project

--- JavaScript/statistics.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("mangoEmoji").innerHTML = `Here are some stats about Mango... ${twemoji.parse("🥭")}`;
-
-setTimeout(function () {
-    document.getElementById("usersNumber").innerHTML = `<i class="fas fa-check-square"></i> More than <span class="counter">100 users ${twemoji.parse("😯")}`;
-    document.getElementById("serversNumber").innerHTML = `<i class="fas fa-check-square"></i> 10 guilds! ${twemoji.parse("👌🏻")}`;
-    document.getElementById("channelsNumber").innerHTML = `<i class="fas fa-check-square"></i> 300 channels... and counting! ${twemoji.parse("😄")}`;
-    document.getElementById("emojisNumber").innerHTML = `<i class="fas fa-check-square"></i> 150 emojis ${twemoji.parse("❤")}`;
-}, 1500);
